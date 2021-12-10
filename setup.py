c38a607753250ec7ac1bcccabc0e949f0ba1abc2
Harold Solbrig <solbrig@jhu.edu>
2021-12-10 12:34:29 -0600

    Update setup.py
    Gratuitous change to get github to revisit the dependendcy graph

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 from setuptools import setup
 
+# The name parameter is required if github is going to create dependency graphs
 NAME = "monomorphic"
 setup(
     name = NAME,
